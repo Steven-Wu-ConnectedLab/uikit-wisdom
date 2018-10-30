44e88facebad8a0c1c6d17ed2c8daa1314a5917c
Steven Wu <swu@connectedlab.com>
2018-10-30 16:23:33 -0400

    SW – creating a IpUIKitWisdom.xcodeproj so that IP-UIKit-Wisdom can be consumed via Carthage.

---
+++ IpUIKitWisdom/IpUIKitWisdom.h
@@ -0,0 +1,19 @@
+//
+//  IpUIKitWisdom.h
+//  IpUIKitWisdom
+//
+//  Created by cl-dev on 2018-10-30.
+//  Copyright © 2018 Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IpUIKitWisdom.
+FOUNDATION_EXPORT double IpUIKitWisdomVersionNumber;
+
+//! Project version string for IpUIKitWisdom.
+FOUNDATION_EXPORT const unsigned char IpUIKitWisdomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IpUIKitWisdom/PublicHeader.h>
+
+
